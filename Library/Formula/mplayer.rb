2ad3b6e7f6e4ad0b0b241369a9a3b6508d1ffc3c
Zhong Jianxin (azuwis) <azuwis@gmail.com>
2010-01-09 22:15:45 +0800

    tweak MPlayer
    * disable gl x11 openal mencoder apple-remote
    * let mplayer choose cflags automatically

--- Library/Formula/mplayer.rb
+++ Library/Formula/mplayer.rb
@@ -14,10 +14,15 @@ class Mplayer <Formula
     # Using gcc_4_2 doesn't work, failing mysteriously (check this again)
     ENV['CC'] = ''
     ENV['LD'] = ''
-    # any kind of optimisation breaks the build
-    ENV['CFLAGS'] = '-w -pipe'
+    # Let MPlayer choose cflags automatically
+    ENV['CFLAGS'] = ''
 
     args = ['./configure', "--prefix=#{prefix}"]
+    args << "--disable-gl"
+    args << "--disable-x11"
+    args << "--disable-openal"
+    args << "--disable-mencoder"
+    args << "--disable-apple-remote"
     args << "--target=x86_64-Darwin" if Hardware.is_64_bit? and MACOS_VERSION >= 10.6
 
     system *args 
